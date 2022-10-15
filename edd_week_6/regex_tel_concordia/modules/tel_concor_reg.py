6e428b0f959fbe718ee61a57035a7f816a3ee490
Morandev <moranfcnddev@gmail.com>
2022-10-15 00:20:05 -0300

    tp3-e1-regex-concordia-area-code-fixed

--- edd_week_6/regex_tel_concordia/modules/tel_concor_reg.py
+++ edd_week_6/regex_tel_concordia/modules/tel_concor_reg.py
@@ -2,7 +2,7 @@ import re
 
 
 def has_concordia_area_code(tel_number: str) -> bool:
-    pattern = '(^.54)[0-9]{11}|(^0?345.)[0-9]{6}$|(^\(0345\))\s[0-9]{9}$'
+    pattern = '(^.549)[0-9]{10}|(^345.)[0-9]{6}$|(^\(0345\))\s[0-9]{9}$'
     pattern_compiled = re.compile(pattern)
 
     return pattern_compiled.match(tel_number) and True
